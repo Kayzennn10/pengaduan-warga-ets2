0445ccf5a24ec468c1da05b0d8f26300a162b952
Kayzennn10 <azkahanif293@gmail.com>
2026-05-08 17:37:22 +0700

    Fix: Numpang di database mysql bawaan RDS

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ db_config = {
     'host': os.getenv('RDS_HOST'),
     'user': os.getenv('RDS_USER'),
     'password': os.getenv('RDS_PASSWORD'),
-    'db': 'pengaduan_db',
+    'db': 'mysql',
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
@@ -28,19 +28,12 @@ s3 = boto3.client(
 BUCKET_NAME = 'pengaduan-desa-azka-2026'
 
 def init_db():
-    """Fungsi otomatis untuk membuat database dan tabel jika belum ada"""
     try:
-        # Koneksi awal tanpa nama database untuk membuat database-nya dulu
-        conn = pymysql.connect(
-            host=db_config['host'],
-            user=db_config['user'],
-            password=db_config['password']
-        )
+        # Koneksi langsung menggunakan database default dari .env
+        conn = pymysql.connect(**db_config)
         cursor = conn.cursor()
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_config['db']}")
-        cursor.execute(f"USE {db_config['db']}")
         
-        # Buat tabel laporan
+        # Langsung buat tabelnya saja
         create_table_query = """
         CREATE TABLE IF NOT EXISTS laporan (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -52,9 +45,9 @@ def init_db():
         """
         cursor.execute(create_table_query)
         conn.commit()
-        print("✅ Database & Tabel berhasil diinisialisasi!")
+        print("✅ Tabel laporan berhasil dipastikan ada!")
     except Exception as e:
-        print(f"❌ Gagal inisialisasi DB: {e}")
+        print(f"❌ Gagal buat tabel: {e}")
     finally:
         conn.close()
 
